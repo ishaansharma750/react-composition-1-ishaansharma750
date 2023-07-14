0372eaffcab9e8141f0ef541e30cd26fdeaceb30
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-07-14 11:54:41 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from "./Tabs";
+
+const item=[{title:"Tab 1",content:"This is the content for Tab 1"},
+            {title:"Tab 2",content:"This is the content for Tab 2"},
+            {title:"Tab 3",content:"This is the content for Tab 3"}
+           ]
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tabs items={item}/>
     </div>
   )
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const Tabs = ({items}) => {
+    console.log(items[0].content);
+    const [active,setActive] = useState(0);
+    console.log(active);
+
+  return (
+    <div>
+        <ul>
+            {items.map((element,index) =>(
+                    <li key="index" onClick={()=>{setActive(index)}}>
+                    {element.title}
+                </li>
+                ))
+            }
+        </ul>
+        {items[active].content}
+    </div>
+  )
+}
+
+export default Tabs
